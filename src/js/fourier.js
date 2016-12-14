69b8e0f0ea594bca81d957299c87e4ed80d08821
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2016-12-14 00:31:38 -0700

    Inverse works for the complex_exponential basis function

--- src/js/fourier.js
+++ src/js/fourier.js
@@ -1,6 +1,25 @@
 const math = require("mathjs");
 
 /**
+ * Calculates the pseudoinverse of a matrix
+ * @param {math.Matrix|number[]} matrix
+ * @returns {math.Matrix|number[]} The pseudoinverse
+ */
+const pinv = function(matrix) {
+    try {
+        return math.multiply(math.conj(math.transpose(matrix)), math.inv(math.multiply(matrix, math.conj(math.transpose(matrix)))));
+    }
+    catch(e) {
+        if (e.message === "Cannot calculate inverse, determinant is zero") {
+            return math.transpose(math.zeros(math.size(matrix)));
+        }
+        else {
+            throw e;
+        }
+    }
+}
+
+/**
  * Possible choices for the basis_function parameter of fourier()
  */
 const basis_functions = {
@@ -25,7 +44,7 @@ const basis_functions = {
  *     the function is sampled at 2*pi/input.length, so the x-value for each
  *     element is 2*pi/i, where i is the element's index.
  *
- * @param {string} [basis_function="sin"]
+ * @param {string} [basis_function="complex_exponential"]
  *     The function on which to project the input function, i.e. the function
  *     that forms the basis for the frequency space. One of "complex_exponential",
  *     "sin", "cos".
@@ -38,9 +57,9 @@ const basis_functions = {
  *     value of the input) and each subsequent element has a higher frequncy.
  */
 const fourier = function(y_values, basis_function) {
-    basis_function = basis_function || "sin";
+    basis_function = basis_function || "complex_exponential";
     if (!(typeof basis_functions[basis_function] === "function")) {
-        throw new Error("Invalid c_function: " + basis_function);
+        throw new Error("Unknown basis function: " + basis_function);
     }
     const x_values = [];
     const step = (2*math.pi)/y_values.length;
@@ -48,14 +67,35 @@ const fourier = function(y_values, basis_function) {
     for (let i = 0; i < y_values.length*step; i+=step) {
         x_values.push(i);
     }
-    const x_values_matrix = math.transpose(math.matrix([x_values]));
-    const out = [];
+    const x_values_matrix = math.matrix([x_values]);
+    const amplitudes = [];
+    x_values.forEach(function(value, n) {
+        const basis = basis_functions[basis_function](math.conj(math.transpose(x_values_matrix)), n);
+        const amplitude = math.squeeze(math.multiply(math.matrix([y_values]), basis));
+        amplitudes.push(math.multiply(0.25, amplitude));
+    });
+    return amplitudes;
+};
+
+const inverse_fourier = function(amplitudes, basis_function) {
+    basis_function = basis_function || "complex_exponential";
+        if (!(typeof basis_functions[basis_function] === "function")) {
+        throw new Error("Unknown basis function: " + basis_function);
+    }
+    const x_values = [];
+    const step = (2*math.pi)/amplitudes.length;
+    // Assume the the x-values are evenly spaced from 0 to 2*pi
+    for (let i = 0; i < amplitudes.length*step; i+=step) {
+        x_values.push(i);
+    }
+    const x_values_matrix = math.matrix([x_values]);
+    const y_values = [];
     x_values.forEach(function(value, n) {
         const basis = basis_functions[basis_function](x_values_matrix, n);
-        const magnitude = math.squeeze(math.multiply(math.matrix([y_values]), basis));
-        out.push(math.multiply(0.25, magnitude));
+        const y_value = math.squeeze(math.multiply(4, math.multiply(amplitudes, pinv(basis))));
+        y_values.push(y_value);
     });
-    return out;
-}
+    return y_values;
+};
 
-module.exports = fourier;
+module.exports = {fourier, inverse_fourier};
